86ecfd323b87979d59052470dc09b0c2a9f6ff8b
hi-lee-mon <shunmel.09@gmail.com>
2023-11-04 15:19:11 +0900

    🐛 inputの値を取得できない不具合を修正

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -3,7 +3,7 @@ import React, { ComponentProps } from 'react';
 const Add = () => {
   const handleAddTodoSubmit: ComponentProps<'form'>['onSubmit'] = (e) => {
     e.preventDefault();
-    const text = e.currentTarget.text;
+    const text = e.currentTarget.text.value;
     e.currentTarget.reset();
   };
 
@@ -11,7 +11,12 @@ const Add = () => {
     <div>
       <h3 className="text-3xl mb-4">TODO追加</h3>
       <form onSubmit={handleAddTodoSubmit} className="border-2 rounded-md p-6">
-        <input type="text" required className="border-2 rounded-md bg-slate-300 h-10 px-2" />
+        <input
+          type="text"
+          name="text"
+          required
+          className="border-2 rounded-md bg-slate-300 h-10 px-2"
+        />
         <button
           type="submit"
           className="px-9 py-2 text-black ml-4 rounded-md bg-cyan-700 transition-all hover:bg-cyan-800  duration-300"
