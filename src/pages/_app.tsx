9b134d3df2017639846d1252f4cd314ffe43f778
hi-lee-mon <shunmel.09@gmail.com>
2023-11-13 02:31:08 +0900

    [add]reduxとredux-toolkitの違いを説明

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,13 @@ const inter = Inter({ subsets: ['latin'] });
  * 【依存関係について】
  * @reduxjs/toolkitの中にreduxが含まれているためreduxは依存関係から削除
  */
+/**
+ * 【redux-toolkitとreduxの違い】
+ * ・sliceを使うことでaction typeやaction creatorを自動生成することができる
+ * ・sliceを使うことでreducer関数を自動生成することができる(ただし内部の処理は自分で記述する必要がある)
+ * ・immerを使うことでreducer関数内でstateを直接変更することができる
+ * ※あとはreduxと同じ
+ */
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
